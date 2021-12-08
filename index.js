a8b84aaf0c88b29f770dbfd9b84ff782a6d1117d
Prashant Chauhan <pnc439@gmail.com>
2021-12-08 23:52:12 +0530

    button created and event listner added

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-alert("Hiii, you are welcome here... :)")
-prompt("Give your favourite number.");
+// alert("Hiii, you are welcome here... :)")
+// prompt("Give your favourite number.");
+
+const button = document.querySelector("#btn")
+button.addEventListener("click",()=> console.log("clicked"))
