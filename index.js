a16bdbb20abffee3df05e8a2b78538f895f3cef2
Prashant Chauhan <pnc439@gmail.com>
2021-12-10 00:33:53 +0530

    Created translation app
    created function for api call
    fetch data and take translated text and added to the page using js

--- index.js
+++ index.js
@@ -5,7 +5,29 @@ const readValue = () => document.querySelector("#txtarea").value;
 
 const button = document.querySelector("#btn");
 
-button.addEventListener("click", () => console.log(readValue()));
+const translate = () => {
+    let line = readValue();
+    fetch("https://api.funtranslations.com/translate/yoda.json?text=" + line)
+        .then(res => res.json())
+        .then(data => (document.querySelector(".result").innerText = data.contents.text))
+        .catch(err => console.log(err))
+}
+// const translate = () => {
+//     let line = readValue();
+//     fetch("https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=" + line)
+//         .then(res => res.json())
+//         .then(data => (document.querySelector(".result").innerText = data.contents.text))
+//         .catch(err => console.log(err))
+// }
+
+button.addEventListener("click", () => translate());
+
+// let urlYoda = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+// fetch(urlYoda + "?text=greate things takes time")
+//     .then(res => res.json())
+//     .then(res => console.log(res.contents.text))
+
 
 // console.log(document.querySelector("textarea"))
 // console.log(document.querySelector(".btn"))
