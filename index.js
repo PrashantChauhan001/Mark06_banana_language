91e1e904c7f90440eab5aba14fb4cab73b5d29b9
Prashant Chauhan <pnc439@gmail.com>
2021-12-09 17:37:59 +0530

    testing with account
    testing

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const readValue = () => document.querySelector("#txtarea").value;
 
 const button = document.querySelector("#btn");
 
-button.addEventListener("click",()=> console.log(readValue()));
+button.addEventListener("click", () => console.log(readValue()));
+
+console.log("test")
 
 // console.log(document.querySelector("textarea"))
 // console.log(document.querySelector(".btn"))
